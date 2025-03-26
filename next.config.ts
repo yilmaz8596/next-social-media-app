33cd2bd8cdacfbfc787a2b284a7def57fd5d2c46
Fırat Yılmaz <yilmaz8596@gmail.com>
2025-03-26 21:02:41 +0300

    update next.config and ts.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // This will ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
